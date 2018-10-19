ac44c44ef6c3a3e79e6bb9d921ea4b2786492f14
Ant <azucaro@gmail.com>
2018-10-19 18:02:44 -0400

    Make the for loop look like it does in master.

--- fragcenter.go
+++ fragcenter.go
@@ -175,10 +175,8 @@ func writeHTML(streams []string, host string, port string) error {
     <q><streamName></q>
   </div>`
 
-	for count, name := range streams {
-		if count < 3 {
-			bodyLines = append(bodyLines, strings.Replace(strings.Replace(strings.Replace(baseVideo, "<streamName>", name, -1), "<stereamHost>", host, -1), "<streamPort>", port, -1))
-		}
+	for _, name := range streams {
+		bodyLines = append(bodyLines, strings.Replace(strings.Replace(strings.Replace(baseVideo, "<streamName>", name, -1), "<stereamHost>", host, -1), "<streamPort>", port, -1))
 	}
 
 	htmlBody = strings.Join(bodyLines, "\n")
